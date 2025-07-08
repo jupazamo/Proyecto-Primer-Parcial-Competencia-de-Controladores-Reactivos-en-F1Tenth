9d631b02f2514d55248e4bab81565828e2d8e3fb
jupazamo <jupazamo@espol.edu.ec>
2025-07-07 22:30:18 -0500

    F1Tenth: primera versión del controlador y launch files

---
+++ src/gap_follow_pkg/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'gap_follow_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        # índice del paquete
+        ("share/ament_index/resource_index/packages",
+         ["resource/" + package_name]),
+        # package.xml
+        ("share/" + package_name, ["package.xml"]),
+        # todos los launch del sub-directorio
+        ("share/" + package_name + "/launch",
+         glob(os.path.join("launch", "*.py"))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jupazamo',
+    maintainer_email='jupazamo@todo.todo',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+    'console_scripts': [
+        'gap_follower = gap_follow_pkg.gap_follower:main',
+        'trajectory_logger = gap_follow_pkg.trajectory_logger:main',
+        "lap_timer    = gap_follow_pkg.lap_timer:main",
+        ],
+    },
+)
